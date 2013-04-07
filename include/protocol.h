65b7c0d0d1701cda4d799f9bf6c7cede62988f72
Tung Dao <me@tungdao.com>
2013-04-07 09:55:40 +0700

    more on protocol

--- include/protocol.h
+++ include/protocol.h
@@ -3,35 +3,67 @@
 #define _PROTOCOL_H_
 
 #define PORT 5000
-#define MAX_USERNAME_LEN 12
+#define MAX_NAME_LEN 32
 #define BUFFER_SIZE 1024
 
 typedef enum MessageType_ {
-    TEST
+    // Test
+    MSG_TEXT,
+
+    // Client
+    MSG_CONNECT, MSG_DISCONNECT,
+    MSG_BEGIN, MSG_END,
+    MSG_INVITE, MSG_REPLY,
+    MSG_TURN, MSG_MOVE,
+    MSG_LIST_PLAYERS,
+
+    // Server
+    MSG_ERROR,
+    MSG_PLAYERS,
 } MessageType;
 
-typedef struct TestMessage_ {
-    char message[BUFFER_SIZE];
-} TestMessage;
+typedef enum Result_ {
+    WIN, LOSE
+} Result;
 
-typedef struct AuthenticationMessage_ {
-    char username[MAX_USERNAME_LEN];
-} AuthenticationMessage;
+
+typedef struct TextMessage_ {
+    char content[BUFFER_SIZE];
+} TextMessage;
+
+typedef struct ConnectMessage_ {
+    char name[MAX_NAME_LEN];
+} ConnectMessage;
+
+typedef struct PlayersMessage_ {
+    int ids[20];
+    char names[20][MAX_NAME_LEN];
+} PlayersMessage;
+
+typedef struct InviteMessage_ {
+    int id;
+} InviteMessage;
 
 typedef struct MoveMessage_ {
-    char username[MAX_USERNAME_LEN];
+    int x;
+    int y;
 } MoveMessage;
+
+typedef struct EndMessage_ {
+    Result result;
+} EndMessage;
 
 typedef struct Message_ {
     MessageType type;
     union {
-        TestMessage test_payload;
-        AuthenticationMessage authentication_payload;
-        MoveMessage move_payload;
+        TextMessage text;
+        ConnectMessage connect;
+        PlayersMessage players;
+        InviteMessage invite;
+        MoveMessage move;
+        EndMessage end;
+        TextMessage error;
     };
 } Message;
-
-Message new_authentication_message(char* username);
-Message new_move_message(char* name);
 
 #endif
